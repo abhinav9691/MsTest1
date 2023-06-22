b12b94494937b15446dcbef8070a55a8afedcfbf
abhinav9691 <81373991+abhinav9691@users.noreply.github.com>
2023-06-22 10:32:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using NlogDemo;
+
+Sum sm = new Sum();
+sm.Add(0, 0);
---
+++ Sum.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo
+{
+    internal class Sum
+    {
+        public static readonly Logger logger =
+            LogManager.GetCurrentClassLogger();
+
+        public void Add(int a,int b)
+        {
+            if(a==0 || b == 0)
+            {
+                Console.WriteLine("a and b values are zero");
+                logger.Error("a and b values are zero");
+            }
+            else
+            {
+                int c = a + b;
+                Console.WriteLine("Addition=" + c);
+                logger.Info("Addition=" + c);
+            }
+        }
+    }
+}
